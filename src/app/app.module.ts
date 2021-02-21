bbb298c27d3b8939e4f5b2243a8c1169db880765
Your Name <you@example.com>
2021-02-21 14:21:34 +0300

    Angular Directives

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ProductComponent } from './product/product.component';
+import {FormsModule} from '@angular/forms';
+import { InputEmailDirective } from './input-email.directive'
+
+@NgModule({
+  declarations: [
+  ProductComponent,
+  InputEmailDirective],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule { }
